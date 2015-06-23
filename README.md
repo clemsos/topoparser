50e434c70fb7c0e63be57696e10243bc5346807b
Clément Renaud <clement.renaud@gmail.com>
2015-06-23 15:21:59 +0200

    v0.2 and updated README

--- README.md
+++ README.md
@@ -4,6 +4,127 @@ Command-line parser for [Topogram](http://github.com/topogram/topogram).
 
 Use a YAML file to describe data source and workflow
 
-Usage
+## Usage
+
+Use the command line to call a YAML file containing the parsing instructions  
 
     python topoparser.py weibo.yaml
+
+You can save the output to json file by specficing and ```--output-dir``` params
+
+    python topoparser.py --output-dir test weibo.yaml
+
+
+## YAML command file structure
+
+The overall workflow is separated in 3 main steps :
+
+1. **corpora** : load and describe the corpus
+2. **process** : extract information from the data
+3. **viz** : parse the data properly  (JSON output)
+
+
+#### Corpora
+
+The corpora description follow a standard model  
+
+    corpora : 
+        - content:
+            type : csv # could be a mongo or dict adapter
+            file : 'examples/sampleweibo.csv' # path of the file
+            columns:  # name of the relevant columns or fields
+                source : uid
+                text : text
+                timestamp : created_at
+                time_pattern : '%Y-%m-%d %H:%M:%S'
+                data : [ permission_denied, deleted_last_seen ]
+
+Multiple corpora can be used together (not implemented yet)
+
+#### Process
+
+
+You first select the column you want to process from the data set -- ex . ```content.text```
+
+The process remains on several data processors :
+
+* **regexp** : compile a regexp  -- ex. ```regexp : '@([^:：,，\)\(（）|\\\s]+)' ``` extract @ mentions from Twitter-like corpus
+* **nlp** : extract keywords from a specific languages  -- ex. ``` nlp : zh ``` extract words from Chinese language
+* **graph** :  will add a list of elements into a graph -- ex. ``` graph : add_edges_from_nodes_list ```
+* **timeseries** : will format time information following specific time scales (second, minute, hour, day, month, year) -- ex. ``` timeseries : minute ```
+
+
+You can use  2 different operators to link them together :
+
+**save** : will store the results of the operation 
+    -- ex. extract all mentions and keep the results 
+
+    - mentions : 
+            regexp : '@([^:：,，\)\(（）|\\\s]+)'
+            type : save 
+
+
+**pipe** : will pass the data to the next operations ( like unix ```|``` symbol )
+    -- ex. extract all keywords from a Chinese sentence and add words  into a network
+
+    - words :
+        nlp : "zh"
+        type : pipe
+    - words_relationships :
+        graph: add_edges_from_nodes_list
+        type : save
+
+
+**Complete example**
+
+This will extract hashtags, mentions and urls + create a network of words + compute a daily timeseries from the quantity of messages 
+
+    process: 
+        content.text :
+            - hashtags :
+                regexp: '#([^#\s]+)#'
+                type : save
+            - mentions : 
+                regexp : '@([^:：,，\)\(（）|\\\s]+)'
+                type : save 
+            - urls: 
+                regexp : '\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^\p{P}\s]|/)))'
+                type : save
+            - words :
+                nlp : "zh"
+                type : save
+            - words :
+                nlp : "zh"
+                type : pipe
+            - words_relationships :
+                graph: add_edges_from_nodes_list
+                type : save
+        content.time :
+            - timecount: 
+                timeseries: day
+                type : save
+
+
+## Visualization parsers
+
+The final step is to parse into JSON formatted properly for visualization library (like d3js)
+
+Currently available : timeseries and network. 
+
+    viz :
+        timeseries:
+            data : content.time.timecount
+    network : 
+        nodes : content.text.words
+        edges : content.text.words_relationships 
+
+
+## TODO
+
+* Use multiple datasets
+* Additional visualization models (map & network+map)
+* New data operators like ```fork``` for parrallel processing 
+* Support for custom scripts and  operations 
+
+
+Project inspired by [Datscript](https://github.com/datproject/datscript/) 
--- topoparser.py
+++ topoparser.py
@@ -3,22 +3,28 @@
 import yaml
 import optparse
 import importlib
+import json, os
+
 from topogram.corpora.csv_file import CSVCorpus
+
 from topogram.processors.regexp import Regexp
 from topogram.processors.nlp import NLP
 from topogram.processors.graph import Graph
-from topogram.vizparsers.network import Network
+from topogram.processors.time_rounder import TimeRounder
 
+from topogram.vizparsers.network import Network
+from topogram.vizparsers.time_series import TimeSeries
+
+# parse command line
 parser = optparse.OptionParser()
 
-parser.add_option('-q', '--query',
-    action="store", dest="query",
-    help="query string", default="spam")
+parser.add_option('-o', '--output-dir',
+    action="store", dest="output",
+    help="Store to file", default="stdout")
 
 options, args = parser.parse_args()
 
 for f in  args : 
-    
     with open(f, 'r') as stream:
         raw = yaml.load(stream)
 
@@ -42,76 +48,184 @@ for f in  args :
                     corpora[corpus_name].validate()
                 except ValueError, e:
                     print e.message, 422
+
             elif c["type"] == "csv_nodes":
                 pass
             else : 
                 raise NotImplementedError("Data type is not available yet.")
 
 
-        # parse pipeline
+
+        # init pipeline
         pipeline = {}
 
-        for path in raw["process"].keys() : 
-            
-            pipeline[path] = [] # ordered actions to take 
+        # fill pipeline with empty values to create its structure 
+        paths = [path for path in raw["process"].keys()]
+        for path in paths : 
+            corpus_name = path.split(".")[0]
 
+            # create each empty corpus
+            try :
+                pipeline[corpus_name]
+            except :
+                pipeline[corpus_name] = {}
+
+            # init columns
+            column_name = path.split(".")[1]
+            pipeline[corpus_name][column_name] = [] # ordered actions to take 
+
+        # start to loop
+        for path in paths : 
+
+            # extract important values from name 
+            corpus_name = path.split(".")[0]
+            column_name = path.split(".")[1]
+
+            #
             for i, command in  enumerate(raw["process"][path]):
-                for com in command:
-                    action =  command[com].keys()[0]
 
+                # find each command
+                for com in command:
+
+                    # get action name
+                    action =  [c for c in command[com].keys() if c != "type"][0] 
+
+                    # parse command
                     todo = {}
+                    todo["name"] = com
+                    todo["type"] =  command[com]["type"]
+                    todo["index"] = i
+
+                    # bind relevant processor class
                     if action == "regexp":
                         r= command[com]["regexp"]
-                        todo["name"] = com
                         todo["command"] = Regexp(r.encode("utf-8"))
-                        todo["type"] =  command[com]["type"]
-                        todo["index"] = i
                     elif action == "nlp":
                         language = command[com]["nlp"]
-                        todo["name"] = com
                         todo["command"] = NLP(language)
-                        todo["type"] =  command[com]["type"]
-                        todo["index"] = i
                     elif action == "graph":
-                        todo["name"] = com
                         run = command[com]["graph"]
                         todo["command"] = Graph()
-                        todo["type"] =  command[com]["type"]
-                        todo["index"] = i
+                    elif action == "timeseries":
+                        timescale = command[com]["timeseries"]
+                        todo["command"] = TimeRounder(timescale)
                     elif action == "run":
-                        todo["name"] = com
                         todo["command"] = command[com]["run"]
-                        todo["type"] =  command[com]["type"]
-                        todo["index"] = i
-                    
-                    pipeline[path]. append(todo)
+
+                    # add to pipeline 
+                    if todo != {} : 
+                       pipeline[corpus_name][column_name].append(todo)
+
+        print pipeline # for monitoring
 
         # parse viz
-        viz = {}
+        vizs = []
+        for viz_type in raw["viz"]: 
+            # print "viz ", viz_type
 
-        for viz_type in raw["viz"].keys() : 
+            # create new model
+            viz = {}
+            
+
+            # add data
+            viz['data'] = raw["viz"][viz_type]
+            viz['name'] = viz_type
+
+            # parse visualizer
             if viz_type  == "network" : 
-                Network()
+                viz["command"] = Network()
+            elif  viz_type == "timeseries" : 
+                viz["command"] = TimeSeries()
             elif  viz_type == "map" : 
                 pass
             elif  viz_type == "network_map" : 
                 pass
             else : 
                 raise NotImplementedError("Viz model not implemented")
+            
+            # stock all vizs
+            vizs.append(viz)
+
+        print vizs # for monitoring
 
 
-print corpora
-print pipeline
+# init data processing
+results = {}
 
-for path in pipeline: 
-    corpus_name = path.split(".")[0]
-    column_name = path.split(".")[1]
+# loop through the whole pipeline, corpus by corpus 
+for corpus_name in pipeline :
 
+    # load corpus
     corpus = corpora[corpus_name]
-    commands = pipeline[path]
-    for row in corpus :
-        data = row[column_name+"_column"]
-        for com in commands: 
-            print com["command"](data)
 
+    piped = False # checked if previously pipe
+
+    # loop through corpus
+    for row in corpus : 
+
+
+        # for each colum
+        for column_name in pipeline[corpus_name]:
+
+            # apply each action
+            commands = pipeline[corpus_name][column_name]
+            
+            # print commands
+            for command in commands: 
+
+                # implement basic pipe process (use the precedent result) 
+                if piped is True: 
+                    result = command["command"](result)
+                    piped = False
+                else :
+                    try :
+                        result = command["command"](row[column_name+"_column"])
+                    except :
+                        print "Processing error with %s" %command["command"]
+                        result = []
+
+                # 
+                if command["type"] == "pipe": piped = True
+                elif command["type"] == "save": 
+                    if result != [] : 
+                        path = corpus_name+"."+column_name+"."+command["name"]
+                        try : 
+                            results[path]
+                        except : 
+                            results[path] = [] 
+                        results[path] += result
+
+                elif com["type"] == "exclude":
+                    pass
+                else : 
+                    raise NotImplementedError("not implemented")
+
+
+# check results
+for res in results: 
+    print res, len(results[res])
+
+# parse for visualizations
+for viz in vizs:
+
+    # build a tuples containing arguments of the function
+    names = [ viz["data"][field]   for field in viz["data"] ]
+
+    args = tuple([results[arg] for arg in names])
+    viz["command"]( *(args)  ) # call the function
+
+    #export to JSON 
+    final = viz["command"].to_JSON()
     
+    if options.output == "stdout": 
+        print  final # on the cli
+    else :
+        if not os.path.exists(options.output):
+            os.makedirs(options.output)
+        json_path = os.path.join(options.output, viz["name"]+".json")
+        with open(json_path, "wb") as f :
+            json.dump(final, f)
+
+
+
+
